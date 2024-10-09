0e8480d62f747603e06219b2c9a5321eff0c9854
rifat977 <itscrifat5147@gmail.com>
2024-10-10 01:11:09 +0600

    fix origin issue2

--- quiz/settings.py
+++ quiz/settings.py
@@ -39,7 +39,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'account.middleware.DisableCsrfForAdminMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
